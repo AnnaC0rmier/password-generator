644dc14bb6ed873dbf89fce79a47ab0d22d2c277
AnnaC0rmier <anna.cormier.96@gmail.com>
2023-12-22 20:58:16 -0600

    saving point

--- script.js
+++ script.js
@@ -1,110 +1,57 @@
-// // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
-// my code starts here 
-
-
-var generateBtn = document.querySelector("#generate")
-
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 function buttonClick()
 {
+let choices =
+{
+uppercase: prompt("should the password include uppercase?") === "yes",
+lowercase: prompt("should the password include lowercase?") === "yes",
+special: prompt("should the password include special characters?") === "yes",
+numeric: prompt("should the password include numeric characters?") === "yes",
+numChar: prompt("how many characters should the password be?") === "yes",
+};
 
-let uppercase = prompt("should the password include uppercase?");
-let lowercase = prompt("should the password include lowercase?");
-let special = prompt("should the password include special characters?");
-let numeric = prompt("should the password include numeric characters?");
-let numChar = prompt("how many characters should the password be?");
+console.log("number of characters", choices.numChar);
+return choices;
 
-
-
-if (uppercase === "yes"){
-  return {uppercase};
-
-}
-
-if (lowercase === "yes"){
- return {lowercase}
-}
-
-if (special === "yes"){
- return {special}
-}
-
-if (numeric === "yes"){
- return {numeric}
-}
-  console.log("number of characters", numChar);
 }
 
 generateBtn.addEventListener("click", buttonClick);
 
 
 
-
-
 function writePassword() 
 
 {
-var chosenChar = buttonClick({ uppercase: "yes", lowercase: "yes", special: "yes", numeric: "yes"});
+// var password = generatePassword();
+var passwordText = document.querySelector("#password")
+    
+
+
+
+var chosenChar = buttonClick();
 var upp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var spc = ["!", "@", "#", "$", "%", "^", "&", "*" ];
 var numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var password = [""]
+var password = [""];
 
-
-if (chosenChar.uppercase === "yes") {
+if (chosenChar.uppercase) {
   password = password.concat(upp);
 }
-if (chosenChar.lowercase === "yes") {
-  password = password.concact(low);
+if (chosenChar.lowercase) {
+  password = password.concat(low);
 }
-if (chosenChar.special === "yes") {
+if (chosenChar.special) {
   password = password.concat(spc);
 }
-if (chosenChar.numChar === "yes") {
-  password = password.concat(numChar);
+if (chosenChar.numeric) {
+  password = password.concat(numb);
 }
+passwordText.value = password
+
+console.log(password);
 }
 
-console.log(writePassword(password.join ("")));
-
-
-
-
-
-
-// if (uppercase === "yes"){
-//   return upp[i];
-// }
-// if (lowercase === "yes"){
-//   return low[i];
-// }
-// if (special === "yes"){
-//   return spc[i];
-// }
-// if (numChar === "yes"){
-//   return numb[i];
-// }
-
-// for (let i = 0; i < numcChar, i++){
-
-// }
-
-// console.log(password)
-// }
+generateBtn.addEventListener("click", writePassword);
